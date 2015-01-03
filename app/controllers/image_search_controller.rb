44602d1e0f09967a57cc094b41226891f168f0f9
Vanya Jauhal <vanyajauhal1995@gmail.com>
2015-01-03 12:11:06 +0530

    Fixes #8815 - Corrects variable reference in image search

--- app/controllers/image_search_controller.rb
+++ app/controllers/image_search_controller.rb
@@ -19,9 +19,13 @@ class ImageSearchController < ::ApplicationController
   end
 
   def search_repository
-    images = use_hub? ? hub_search_image(params[:search]) : registry_search_image(params[:search])
+    repositories = use_hub? ? hub_search_image(params[:search]) :
+                              registry_search_image(params[:search])
     respond_to do |format|
-      format.js { render :partial => 'repository_search_results', :locals => { :images => images } }
+      format.js do
+        render :partial => 'repository_search_results',
+               :locals  => { :repositories => repositories }
+      end
     end
   end
 
