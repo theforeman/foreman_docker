unorthodoxgeek/foreman-docker

Fixes #10029 - Docker registry API

app/views/api/v2/docker_registries/index.json.rabl
@@ -1,0 +1,3 @@
+collection @registries
+
+extends 'api/v2/docker_registries/main'

app/views/api/v2/docker_registries/main.json.rabl
@@ -1,0 +1,6 @@
+object @registry
+
+extends 'api/v2/docker_registries/base'
+
+attributes :url, :description, :created_at, :updated_at
+

app/views/api/v2/docker_registries/show.json.rabl
@@ -1,0 +1,7 @@
+object @registry
+
+extends "api/v2/docker_registries/main"
+
+node do |container|
+  partial("api/v2/taxonomies/children_nodes", :object => container)
+end

config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
             end
           end
         end
+        resources :docker_registries, :except => [:new, :edit]
       end
     end
   end

lib/foreman_docker/engine.rb
@@ -79,11 +79,17 @@ module ForemanDocker
         end
 
         security_block :registries do
-          permission :view_registries, { :registries => [:index, :show] },
+          permission :view_registries,
+                     { :registries => [:index, :show],
+                       :'api/v2/docker_registries' => [:index, :show] },
                      :resource_type => 'DockerRegistry'
-          permission :create_registries, { :registries  => [:new, :create, :update, :edit] },
+          permission :create_registries,
+                     { :registries  => [:new, :create, :update, :edit],
+                       :'api/v2/docker_registries' => [:create, :update] },
                      :resource_type => 'DockerRegistry'
-          permission :destroy_registries, { :registries => [:destroy] },
+          permission :destroy_registries,
+                     { :registries => [:destroy],
+                       :'api/v2/docker_registries' => [:destroy] },
                      :resource_type => 'DockerRegistry'
         end
 

theforeman/foreman_docker

app/views/api/v2/registries/index.json.rabl
@@ -1,0 +1,3 @@
+collection @registries
+
+extends 'api/v2/registries/main'

app/views/api/v2/registries/main.json.rabl
@@ -1,0 +1,6 @@
+object @registry
+
+extends 'api/v2/registries/base'
+
+attributes :url, :description, :created_at, :updated_at
+

app/views/api/v2/registries/show.json.rabl
@@ -1,0 +1,7 @@
+object @registry
+
+extends "api/v2/registries/main"
+
+node do |container|
+  partial("api/v2/taxonomies/children_nodes", :object => container)
+end

config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
             end
           end
         end
+        resources :registries, :except => [:new, :edit]
       end
     end
   end

lib/foreman_docker/engine.rb
@@ -79,11 +79,17 @@ module ForemanDocker
         end
 
         security_block :registries do
-          permission :view_registries, { :registries => [:index, :show] },
+          permission :view_registries,
+                     { :registries => [:index, :show],
+                       :'api/v2/registries' => [:index, :show] },
                      :resource_type => 'DockerRegistry'
-          permission :create_registries, { :registries  => [:new, :create, :update, :edit] },
+          permission :create_registries,
+                     { :registries  => [:new, :create, :update, :edit],
+                       :'api/v2/registries' => [:create, :update] },
                      :resource_type => 'DockerRegistry'
-          permission :destroy_registries, { :registries => [:destroy] },
+          permission :destroy_registries,
+                     { :registries => [:destroy],
+                       :'api/v2/registries' => [:destroy] },
                      :resource_type => 'DockerRegistry'
         end
 