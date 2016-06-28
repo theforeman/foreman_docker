692e3a2621d65e0ef3a8cbf928b8d43919d70d3e
Shlomi Zadok <shlomi@ben-hanna.com>
2016-06-28 10:48:44 +0300

    Fixes #15528 - 'command' should not be required

--- app/models/docker_container_wizard_states/configuration.rb
+++ app/models/docker_container_wizard_states/configuration.rb
@@ -4,8 +4,6 @@ module DockerContainerWizardStates
     belongs_to :wizard_state, :class_name => DockerContainerWizardState,
                               :foreign_key => :docker_container_wizard_state_id
 
-    validates :command, :presence => true
-
     attr_accessible :name, :command, :entrypoint, :cpu_set, :cpu_shares, :memory, :wizard_state
   end
 end
